2215767b540ecde3f2ea45fcdcdd4f79a31720ce
PixelT <pixeltownpl@gmail.com>
2017-04-28 22:24:25 +0200

    Update gulpfile + package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,43 @@
-var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    rename       = require('gulp-rename'),
-    browserSync  = require('browser-sync'),
-    autoprefixer = require('gulp-autoprefixer');
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')({pattern: '*'});
 
 // ----- compile sass [expanded] --------------------------
 gulp.task('sass-expanded', function() {
     gulp.src('ug-grid.scss')
-        .pipe(sass({
+        .pipe($.sass({
             'outputStyle': 'expanded',
             'indentWidth': 4
-        }).on('error', sass.logError))
-        .pipe(autoprefixer({
+        }).on('error', $.sass.logError))
+        .pipe($.autoprefixer({
             browsers: ['last 3 versions', 'IE >= 10'],
             cascade: false
         }))
         .pipe(gulp.dest(''))
-        .pipe(browserSync.stream({match: '**/*.css'}));
+        .pipe($.browserSync.stream({match: '**/*.css'}));
 });
 
 // ----- compile sass [compressed] ------------------------
 gulp.task('sass-compressed', function() {
     gulp.src('ug-grid.scss')
-        .pipe(sass({
+        .pipe($.sass({
             'outputStyle': 'compressed'
-        }).on('error', sass.logError))
-        .pipe(autoprefixer({
+        }).on('error', $.sass.logError))
+        .pipe($.autoprefixer({
             browsers: ['last 3 versions', 'IE >= 10'],
             cascade: false
         }))
-        .pipe(rename({
+        .pipe($.rename({
             'suffix': '.min'
         }))
         .pipe(gulp.dest(''))
-        .pipe(browserSync.stream({match: '**/*.css'}));
+        .pipe($.browserSync.stream({match: '**/*.css'}));
 });
 
 // ----- watch demo ---------------------------------------
 gulp.task('watch-demo', ['sass-expanded', 'sass-compressed'], function() {
-    gulp.watch('demo.html').on('change', browserSync.reload);
+    gulp.watch('demo.html').on('change', $.browserSync.reload);
     gulp.watch('**/*.scss', ['sass-expanded', 'sass-compressed']);
-    browserSync.init({
+    $.browserSync.init({
         notify: false,
         server: {
             baseDir: './',
